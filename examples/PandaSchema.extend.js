eedaa830cd07a1971c213073ccf5f1fae861aa12
Markus Dobmann <md@noqq.eu>
2015-07-08 16:00:39 +0200

    Add Schema.extend() example

---
+++ examples/PandaSchema.extend.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var Schema = require("../lib/Schema");
+
+var PandaSchema = new Schema({
+    name: String,
+    age: Number,
+    friends: {
+        type: Array
+    }
+});
+
+var SuperPanda = PandaSchema.extend("SuperPanda", {
+    xRay: true,
+    canFly: {
+        type: Boolean
+    }
+});
